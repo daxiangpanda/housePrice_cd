3a68d4337d51a597fc05af1bb93dbc0721a5d6c0
Ziqih <32486345+Ziqih@users.noreply.github.com>
2018-06-08 13:07:03 +0800

    Update pipelines.py
    增加了保存json文件的功能，来源：http://www.cnblogs.com/juedui0769/p/4839731.html

--- housePrice/housePrice/pipelines.py
+++ housePrice/housePrice/pipelines.py
@@ -4,8 +4,23 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import json
+import codecs
 
 class HousepricePipeline(object):
+
+    def __init__(self):
+        self.file = codecs.open('scraped_data_utf8.json', 'w', encoding='utf-8')
+
     def process_item(self, item, spider):
+        line = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        self.file.write(line)
         return item
+
+    def spider_closed(self, spider):
+        self.file.close()
+#
+# class HousepricePipeline(object):
+#     def process_item(self, item, spider):
+#         return item
+
